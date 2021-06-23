1b68785ccb5de577f6acc0a7b8fee4238c35fde8
Michael Buckley <michael.buckley.806@my.csun.edu>
2021-06-23 15:57:38 -0700

    I made this change

--- Code/app/src/main/java/com/example/qwikcloset/LookUpMaps.java
+++ Code/app/src/main/java/com/example/qwikcloset/LookUpMaps.java
@@ -33,7 +33,7 @@ public class LookUpMaps {
         return output;
     }
 
-    public static void main(String[]args){
+    public static void main(String []args){
         int[] temp = reverseMap("Crop top");
         System.out.println(reverseMap("Crop top"));
     }
