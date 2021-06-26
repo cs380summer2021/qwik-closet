6b81b45dcd250b77db1d48172bef31269048881b
Osher Boudara <osherboudara99@gmail.com>
2021-06-26 15:23:40 -0700

    condensed database and table

--- Code/app/src/main/java/com/example/qwikcloset/DatabaseHelper.java
+++ Code/app/src/main/java/com/example/qwikcloset/DatabaseHelper.java
@@ -8,24 +8,16 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Clothing.db";
-    public static final String TABLE_1 = "Tops_table";
-    public static final String COL_1_TABLE_1 = "Top_ID";
-    public static final String COL_2_TABLE_1 = "Top_Name";
-    public static final String COL_3_TABLE_1 = "Crop Top";
-    public static final String COL_4_TABLE_1 = "T-Shirt";
-    public static final String COL_5_TABLE_1 = "Tank Top";
-    public static final String COL_6_TABLE_1 = "Long Sleeve / Flannel";
-    public static final String COL_7_TABLE_1 = "Collared Shirt";
-    public static final String COL_8_TABLE_1 = "Dress Shirt / Blouse";
-    public static final String COL_9_TABLE_1 = "Long Dress";
-    public static final String COL_10_TABLE_1 = "Medium Dress";
-    public static final String COL_11_TABLE_1 = "Short Dress";
-    public static final String COL_12_TABLE_1 = "Shirt";
-    public static final String COL_13_TABLE_1 = "Dress";
-    public static final String COL_14_TABLE_1 = "Bottoms Required?";
-    public static final String TABLE_2 = "Bottoms_table";
-    public static final String TABLE_3 = "Shoes_table";
-    public static final String TABLE_4 = "Accessories_table";
+    public static final String TABLE_1 = "Clothing_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "Category";
+    public static final String COL_3 = "Sub_Category";
+    public static final String COL_4 = "Specific_Category";
+    public static final String COL_5 = "Mood";
+    public static final String COL_6 = "Weather";
+    public static final String COL_7 = "Task";
+    public static final String COL_8 = "Color";
+
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
         SQLiteDatabase db = this.getWritableDatabase();
@@ -33,7 +25,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_1 + " (Top_ID INTERGER PRIMARY KEY AUTOINCREMENT, Top_Name TEXT, Crop_Top BOOLEAN, TShirt BOOLEAN, TankTop BOOLEAN, Long_Sleeve_Flannel BOOLEAN, Collared_Shirt BOOLEAN, Dress_Shirt_Blouse BOOLEAN, Long_Dress BOOLEAN, Medium_Dress BOOLEAN, Short_Dress BOOLEAN, Shirt BOOLEAN, Dress BOOLEAN, BottomsRequired BOOLEAN)");
+        db.execSQL("CREATE TABLE " + TABLE_1 + " (ID INTERGER PRIMARY KEY AUTOINCREMENT, Category TEXT, Sub_Category TEXT , Specific_Category TEXT, Mood TEXT, Weather TEXT, Task TEXT, Color TEXT)");
 
     }
 
