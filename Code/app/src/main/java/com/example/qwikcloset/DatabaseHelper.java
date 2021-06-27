9a475bf4dc5a92f06a0715af6f4e7712a5161317
Osher Boudara <osherboudara99@gmail.com>
2021-06-26 22:59:40 -0700

    add image field to database

--- Code/app/src/main/java/com/example/qwikcloset/DatabaseHelper.java
+++ Code/app/src/main/java/com/example/qwikcloset/DatabaseHelper.java
@@ -9,14 +9,6 @@ import androidx.annotation.Nullable;
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Clothing.db";
     public static final String TABLE_1 = "Clothing_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "Category";
-    public static final String COL_3 = "Sub_Category";
-    public static final String COL_4 = "Specific_Category";
-    public static final String COL_5 = "Mood";
-    public static final String COL_6 = "Weather";
-    public static final String COL_7 = "Task";
-    public static final String COL_8 = "Color";
 
     public DatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -25,7 +17,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_1 + " (ID INTERGER PRIMARY KEY AUTOINCREMENT, Category TEXT, Sub_Category TEXT , Specific_Category TEXT, Mood TEXT, Weather TEXT, Task TEXT, Color TEXT)");
+        db.execSQL("CREATE TABLE " + TABLE_1 + " (ID INTERGER PRIMARY KEY AUTOINCREMENT, Category TEXT, Sub_Category TEXT , Specific_Category TEXT, Mood TEXT, Weather TEXT, Task TEXT, Color TEXT, Image BLOB)");
 
     }
 
